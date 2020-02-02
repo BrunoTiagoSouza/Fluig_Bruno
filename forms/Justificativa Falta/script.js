613e7d85960d9b0a8b4765e73b6d3929a7775818
BrunoTiagoSouza <brunotiago09@hotmail.com>
2020-02-02 21:37:53 -0200

    create ifs save checkbox choice

--- forms/Justificativa Falta/script.js
+++ forms/Justificativa Falta/script.js
@@ -8,7 +8,7 @@ $(document).ready(function (){
     $("#colab_matricula").mask('000.000');
 
     if(CURRENTE_STATE === 2){
-        
+
     }
     
 });
@@ -27,10 +27,42 @@ function beforeSendValidate(numState, nextState){
         if($('input[name="colab_matricula"]').val() == ""){
             msg += "É necessário preencher o campo matricula.<br>";
         }
+
+
+        if($('input[name="atraso"]:checked').val() === 'atraso'){
+            $('input[name="atraso_hidden"]').val('atraso');
+        }
+        if($('input[name="atraso"]:checked').val() === 'atraso'){
+            $('input[name="atraso_hidden"]').val('atraso');
+        }
+
         if(typeof ext_Permitidas.find(function(ext){
             return arquivo_enviado.val().split('.').pop() == ext; }) == 'undefined'){
             //$('#resultado').text('Arquivo invalido');
             msg += "Arquivo invalido. verifique se o arquivo é jpg, png, gif, pdf, txt, doc, docx.<br>";
+        }
+
+        //guarda a escolha dos checkbox 
+        if($('input[name="atraso"]:checked').val() === 'atraso'){
+            $('input[name="atraso_hidden"]').val('atraso');
+        }
+        if($('input[name="saida_durante_expediente"]:checked').val() === 'saida_durante_expediente'){
+            $('input[name="saidurante_hidden"]').val('saida_durante_expediente');
+        }
+        if($('input[name="saida_antecipada"]:checked').val() === 'saida_antecipada'){
+            $('input[name="saiantes_hidden"]').val('saida_antecipada');
+        }
+        if($('input[name="falta_saida_meio_periodo"]:checked').val() === 'falta_saida_meio_periodo'){
+            $('input[name="falmeio_hidden"]').val('falta_saida_meio_periodo');
+        }
+        if($('input[name="ausencia_marcacao_saida"]:checked').val() === 'ausencia_marcacao_saida'){
+            $('input[name="outro_hidden"]').val('ausencia_marcacao_saida');
+        }
+        if($('input[name="folga"]:checked').val() === 'atraso'){
+            $('input[name="folga_hidden"]').val('atraso');
+        }
+        if($('input[name="falta_ausencia_integral"]:checked').val() === 'falta_ausencia_integral'){
+            $('input[name="falintegral_hidden"]').val('falta_ausencia_integral');
         }
 
     } else if (CURRENT_STATE === 14 ){
