f531239a6ad3367a0d1df2352d1647f19b02b9a0
BrunoTiagoSouza <brunotiago09@hotmail.com>
2020-02-02 18:29:22 -0200

    create validation for anexo

--- forms/Justificativa Falta/script.js
+++ forms/Justificativa Falta/script.js
@@ -11,6 +11,7 @@ $(document).ready(function (){
 
 function beforeSendValidate(currentState, nextStage){
     var msg = "";
+    var ext_Permitidas = ['jpg', 'png', 'gif', 'pdf', 'txt', 'doc', 'docx'];
 
     if(CURRENT_STATE === 1 || CURRENT_STATE === 0){
         if($('input[name="colab_nome"]').val() == ""){
@@ -22,10 +23,26 @@ function beforeSendValidate(currentState, nextStage){
         if($('input[name="colab_matricula"]').val() == ""){
             msg += "É necessário preencher o campo matricula.<br>";
         }
+        if(typeof ext_Permitidas.find(function(ext){
+            return arquivo_enviado.val().split('.').pop() == ext; }) == 'undefined'){
+            //$('#resultado').text('Arquivo invalido');
+            msg += "Arquivo invalido. verifique se o arquivo é jpg, png, gif, pdf, txt, doc, docx.<br>";
+        }
 
-        
     } else if (CURRENT_STATE === 2 ){
 
     }
 
 };
+
+/*function validaAnexo(){
+    var arquivo_enviado = $("#enviar_arquivo").val();
+    var ext_Permitidas = ['jpg', 'png', 'gif', 'pdf', 'txt', 'doc', 'docx'];
+    console.log(arquivo_enviado.get(0).files[0].size);
+
+    //funcao validando presença e extensao do arquivo....
+    
+
+    }
+
+}*/
